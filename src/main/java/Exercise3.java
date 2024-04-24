4a324c35a9a8ba9020ac9438669bdc9c32299399
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-04-24 11:03:02 +0200

    java-reactive-example script update

---
+++ src/main/java/Exercise3.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise3 {
+    public static void main(String[] args) throws IOException {
+        // Use ReactiveSources.intNumbersFlux
+
+        // Get all numbers in the ReactiveSources.intNumbersFlux stream
+        // To put them into a liste and print the list and its size
+        List<Integer> numbers = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println("list= " + numbers);
+        System.out.println("list.size()= " + numbers.size());
+    }
+}
