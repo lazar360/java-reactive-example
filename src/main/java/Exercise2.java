98496dc20a9d08a3bb53ad0380577b12114172e4
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-04-24 10:22:48 +0200

    java-reactive-example script update

--- src/main/java/Exercise2.java
+++ src/main/java/Exercise2.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+
 public class Exercise2 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFlux and Reactivesources.userFlux
+
+        // Print all numbers in the ReactiveSources.intNumbersFlux stream
+        ReactiveSources.intNumbersFlux().subscribe(element -> System.out.println(element));
+
+        // The same for users flux
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+
 }
