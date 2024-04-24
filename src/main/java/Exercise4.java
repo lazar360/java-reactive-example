3e67cc99109d8f2d9422e1727126e9dc6ac1515e
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-04-24 11:26:26 +0200

    java-reactive-example script update

---
+++ src/main/java/Exercise4.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Optional;
+
+public class Exercise4 {
+    public static void main(String[] args) throws IOException {
+        // Use ReactiveSources.intNumberMono()
+
+        // Print the value from intNumberMono when it emits
+        ReactiveSources.intNumberMono().subscribe(number-> System.out.println(number));
+
+        // Get the value from the Mono into an Integer Value
+        // Optional<Integer> number = ReactiveSources.intNumberMono().blockOptional();
+        Integer number = ReactiveSources.intNumberMono().block();
+
+        System.out.println("Press a key to end");
+        System.in.read();
+        System.out.println("number= " + number);
+    }
+}
